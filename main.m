cec487e820d29de6b192d29a401bc6c876c7bba8
Spencer R Gariano <sgariano22@my.whitworth.edu>
2022-05-07 17:45:43 -0700

    added more images

--- main.m
+++ main.m
@@ -9,7 +9,9 @@ nb=127; % number of windows; must be > 1
 b=100; % Bits allowed to represent numbers
 L=5; % Quantization interval [-L L]
 q=2*L/(2^b-1); %Quantization number
+dataPoints = 4096;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %MDCT matrix formation
 for i=1:n 
     for j=1:2*n
@@ -19,12 +21,7 @@ end
 M=sqrt(2/n)*M;
 inverseM=M'; % inverse MDCT
 
-Fs=8192; %Sampling rate in (htz?)
-
-%Input data
-f=4;  %frequency
-x=cos((1:4096)*pi*64*f/4096); % test signal
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Construct the H vector
 h = [];
 for k=1:2*n
@@ -33,51 +30,65 @@ for k=1:2*n
 end
 h = h.'; %Transpose h matrix to make multiplication work
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Input data
+frequencies = [11];  %Multiples of 64
+frequencyMatrix = [];
+for index=1:size(frequencies,2) %four tones
+    x=cos((1:dataPoints)*pi*64*(frequencies(index))/dataPoints); % test signal
+    frequencyMatrix = [frequencyMatrix x];
+end
 
-out=[];
-for k=1:nb % loop over windows
+combinedOutputs = [];
+for i=1:size(frequencies,2)
+    out=[];
+    for k=1:nb % loop over windows
+        
+        %Compression
+        tone = frequencyMatrix(((dataPoints*i)-dataPoints+1):dataPoints*i);
+        x0=tone(1+(k-1)*n:2*n+(k-1)*n)';       %Get input vector window (Cutting input into windows)
+        x0 = x0.*h;                         %Transform input vector via H-element wise multiplication
+        y0=M*x0;                            %MDCT matrix multiplication to get output vector
+        y1=round(y0/q);                     %Quantization
     
-    %Compression
-    x0=x(1+(k-1)*n:2*n+(k-1)*n)';       %Get input vector window (Cutting input into windows)
-    x0 = x0.*h;                         %Transform input vector via H-element wise multiplication
-    y0=M*x0;                            %MDCT matrix multiplication to get output vector
-    y1=round(y0/q);                     %Quantization
-    
-    %Decompression
-    y2=y1*q;                            %Dequantization
-    
-    %W is matrix of all windows
-    w(:,k)=inverseM*y2;                 %Set column vectors of W to Inverse M * dequantized output vector
-    w(:,k)= w(:,k).*h;                  %Undo window function - Multiply new column vector by H element wise
-    
-    %Skip first iteration
-    if(k>1)
-        w2=w(1:n,k);                    %Window split bottom half
-        w3=w(n+1:2*n,k-1);              %Window split top half
+        %Decompression
+        y2=y1*q;                            %Dequantization
 
-        %W2 and W3 are 32x1 column vectors
-        out=[out;(w2+w3)/2];            %Reconstructed signal
+        %W is matrix of all windows
+        w(:,k)=inverseM*y2;                 %Set column vectors of W to Inverse M * dequantized output vector
+        w(:,k)= w(:,k).*h;                  %Undo window function - Multiply new column vector by H element wise
+
+        %Skip first iteration
+        if(k>1)
+            w2=w(1:n,k);                    %Window split bottom half
+            w3=w(n+1:2*n,k-1);              %Window split top half
+
+            %W2 and W3 are 32x1 column vectors
+            out=[out;(w2+w3)/2];            %Reconstructed signal
+        end
     end
+    combinedOutputs = [combinedOutputs; out];
 end
 
 %Display plots
 figure(1);
-plot(x);
+plot(frequencyMatrix(1:1:end));
 hold on 
-plot(x, 'o');
-title('Input Data With Even f');
+plot(frequencyMatrix(1:1:end), 'o');
+title('Input Data With f = 11');
 legend('Input Data Points','Fitted wave')
-xlim([0,1000]);
+xlim([0,300]);
 
 figure(2);
-plot(out);
+plot(combinedOutputs(1:1:end));
 hold on
-plot(out, 'o');
-title('Output With Even f');
+plot(combinedOutputs(1:1:end), 'o');
+title('Output With f = 11');
 legend('Reconstructed data points','Fitted wave from points')
-xlim([0,100]);
+xlim([0,300]);
 
 Fs=8192; %Sampling rate in (htz?)
-sound(x,Fs) % Matlab’s sound command
-pause(1)
-sound(out,Fs) % play the reconstructed tone
+%sound(frequencyMatrix,Fs) % Matlab’s sound command
+%pause(1)
+%sound(combinedOutputs, Fs)
+%sound(out,Fs) % play the reconstructed tone
