a8c80c4f5f899ae2d7474efa93697330f2f6ae3c
Spencer R Gariano <sgariano22@my.whitworth.edu>
2022-05-09 16:58:38 -0700

    pushing most recent work

--- code/main.m
+++ code/main.m
@@ -6,9 +6,9 @@ n=32; % length of window
 nb=127; % number of windows; must be > 1
 
 %Quantization information
-b=4; % Bits allowed to represent numbers
-L=5; % Quantization interval [-L L]
-q=2*L/(2^b-1); %Quantization number
+%b=4; % Bits allowed to represent numbers
+%L=5; % Quantization interval [-L L]
+%q=2*L/(2^b-1); %Quantization number
 dataPoints = 4096;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -32,7 +32,7 @@ h = h.'; %Transpose h matrix to make multiplication work
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Input data
-frequencies = [11];  %Multiples of 64
+frequencies = [9,11];  %Multiples of 64
 frequencyMatrix = [];
 for index=1:size(frequencies,2) %four tones
     x=cos((1:dataPoints)*pi*64*(frequencies(index))/dataPoints); % test signal
@@ -40,36 +40,48 @@ for index=1:size(frequencies,2) %four tones
 end
 
 combinedOutputs = [];
+bFD=[];
 for i=1:size(frequencies,2)
     out=[];
     for k=1:nb % loop over windows
         
         %Compression
         tone = frequencyMatrix(((dataPoints*i)-dataPoints+1):dataPoints*i);
-        x0=tone(1+(k-1)*n:2*n+(k-1)*n)';       %Get input vector window (Cutting input into windows)
-        x0 = x0.*h;                         %Transform input vector via H-element wise multiplication
-        y0=M*x0;                            %MDCT matrix multiplication to get output vector
-        y1=round(y0/q);                     %Quantization
-    
+        x0=tone(1+(k-1)*n:2*n+(k-1)*n)';            %Get input vector window (Cutting input into windows)
+        x0 = x0.*h;                                 %Transform input vector via H-element wise multiplication
+        y0=M*x0;                                    %MDCT matrix multiplication to get output vector
+        q =[];
+        y1=[];
+        for j=1:size(y0)
+            b =  ceil(abs(log(abs(y0(j))))) + 1;    %Get b bits for the given y value (importance sampling)
+            bFD=[bFD; b];
+            L = abs(y0(j));                         %Quantization interval [-L L]
+            q = [q; 2*L/(2^b-1)];                   %Quantization number
+        end
+        for f=1:size(q)
+            y1=[y1; round(y0(f)/q(f))];             %Quantization
+            
+        end
         %Decompression
-        y2=y1*q;                            %Dequantization
+        y2=y1.*q;                                   %Dequantization
 
         %W is matrix of all windows
-        w(:,k)=inverseM*y2;                 %Set column vectors of W to Inverse M * dequantized output vector
-        w(:,k)= w(:,k).*h;                  %Undo window function - Multiply new column vector by H element wise
+        w(:,k)=inverseM*y2;                         %Set column vectors of W to Inverse M * dequantized output vector
+        w(:,k)= w(:,k).*h;                          %Undo window function - Multiply new column vector by H element wise
 
         %Skip first iteration
         if(k>1)
-            w2=w(1:n,k);                    %Window split bottom half
-            w3=w(n+1:2*n,k-1);              %Window split top half
+            w2=w(1:n,k);                            %Window split bottom half
+            w3=w(n+1:2*n,k-1);                      %Window split top half
 
             %W2 and W3 are 32x1 column vectors
-            out=[out;(w2+w3)/2];            %Reconstructed signal
+            out=[out;(w2+w3)/2];                    %Reconstructed signal
         end
     end
     combinedOutputs = [combinedOutputs; out];
 end
-
+disp(max(bFD));
+bits = sum(bFD);
 %Display plots
 figure(1);
 plot(frequencyMatrix(1:1:end));
