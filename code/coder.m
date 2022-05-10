d0f6d529399c4b05871295c2e76c16731c92bdac
Spencer R Gariano <sgariano22@my.whitworth.edu>
2022-05-09 17:47:32 -0700

    added code into coder and decoder

--- code/coder.m
+++ code/coder.m
@@ -1,7 +1,45 @@
-function [audioBits,outputArg2] = coder(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [audioBits,y1] = coder(vectorX)
+    n=32; % length of window
+    nb=127; % number of windows; must be > 1
+    dataPoints = 4096;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %MDCT matrix formation
+    for i=1:n 
+        for j=1:2*n
+            M(i,j)= cos((i-1+1/2)*(j-1+1/2+n/2)*pi/n);
+        end
+    end
+    M=sqrt(2/n)*M;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Construct the H vector
+    h = [];
+    for k=1:2*n
+        val = sqrt(2) * sin(((k+0.5)*pi) / (2*n));
+        h = [h val];
+    end
+    h = h.'; %Transpose h matrix to make multiplication work
+
+   bFD = [];
+   for k=1:nb % loop over windows
+        %Compression
+        tone = vectorX(((dataPoints*i)-dataPoints+1):dataPoints*i);
+        x0=tone(1+(k-1)*n:2*n+(k-1)*n)';            %Get input vector window (Cutting input into windows)
+        x0 = x0.*h;                                 %Transform input vector via H-element wise multiplication
+        y0=M*x0;                                    %MDCT matrix multiplication to get output vector
+        q =[];
+        for j=1:size(y0)
+            b =  ceil(abs(log(abs(y0(j))))) + 1;    %Get b bits for the given y value (importance sampling)
+            bFD=[bFD; b];
+            L = abs(y0(j));                         %Quantization interval [-L L]
+            q = [q; 2*L/(2^b-1)];                   %Quantization number
+        end
+        for f=1:size(q)
+            y1=[y1; round(y0(f)/q(f))];             %Quantization
+        end
+   end
+   audioBits=max(bFD);
+
 end
 
--- code/decoder.m
+++ code/decoder.m
@@ -1,7 +1,40 @@
-function [outputArg1,outputArg2] = decoder(inputArg1,inputArg2)
-%DECODER Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [out] = decoder(y1)
+    n=32; % length of window
+    nb=127; % number of windows; must be > 1
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %MDCT matrix formation
+    for i=1:n 
+        for j=1:2*n
+            M(i,j)= cos((i-1+1/2)*(j-1+1/2+n/2)*pi/n);
+        end
+    end
+    M=sqrt(2/n)*M;
+    inverseM=M'; % inverse MDCT
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Construct the H vector
+    h = [];
+    for k=1:2*n
+        val = sqrt(2) * sin(((k+0.5)*pi) / (2*n));
+        h = [h val];
+    end
+    h = h.'; %Transpose h matrix to make multiplication work
+    for k=1:nb
+    %Decompression
+        y2=y1.*q;                                   %Dequantization
+
+        %W is matrix of all windows
+        w(:,k)=inverseM*y2;                         %Set column vectors of W to Inverse M * dequantized output vector
+        w(:,k)= w(:,k).*h;                          %Undo window function - Multiply new column vector by H element wise
+
+        %Skip first iteration
+        if(k>1)
+            w2=w(1:n,k);                            %Window split bottom half
+            w3=w(n+1:2*n,k-1);                      %Window split top half
+
+            %W2 and W3 are 32x1 column vectors
+            out=[out;(w2+w3)/2];                    %Reconstructed signal
+        end
+    end
 end
 
